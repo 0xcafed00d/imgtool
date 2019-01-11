d9a5bc4fcfdb13dbde46198a30e2d52ef868b1c5
Lee Witek <neomech@gmail.com>
2019-01-11 15:27:42 +0000

    impl commands

--- main.go
+++ main.go
@@ -9,6 +9,26 @@ import (
 	"os"
 )
 
+type cmdHandler func(img image.PalettedImage, pal color.Palette) error
+
+type cmd struct {
+	cmdHandler
+	desc string
+}
+
+func xpalhex(img image.PalettedImage, pal color.Palette) error {
+	for _, c := range pal {
+		r, g, b := toRGB(c)
+		fmt.Printf("0x%06x\n", uint32(r)<<16|uint32(g)<<8|uint32(b))
+	}
+
+	return nil
+}
+
+var commands = map[string]cmd{
+	"xpalhex": {xpalhex, "export palatte as 32bit hex values"},
+}
+
 func exitOnError(e error) {
 	if e != nil {
 		abend(e.Error())
@@ -18,6 +38,19 @@ func exitOnError(e error) {
 func abend(msg string) {
 	fmt.Fprintln(os.Stderr, msg)
 	os.Exit(-1)
+}
+
+func toRGB(c color.Color) (r uint8, g uint8, b uint8) {
+	_r, _g, _b, _ := c.RGBA()
+	r = uint8(_r / 0x101)
+	g = uint8(_g / 0x101)
+	b = uint8(_b / 0x101)
+	return
+}
+
+func zeroRGB(c color.Color) bool {
+	_r, _g, _b, _ := c.RGBA()
+	return _r == 0 && _g == 0 && _b == 0
 }
 
 func main() {
@@ -40,21 +73,19 @@ func main() {
 
 	fmt.Println(format, " ", img.Bounds())
 	pimg, ok := img.(image.PalettedImage)
-	if ok {
-		_ = pimg
-	} else {
+	if !ok {
 		abend("Not a palletised image")
 	}
 
 	pal, ok := img.ColorModel().(color.Palette)
-	if ok {
-		for n := 0; n < len(pal); n++ {
-			r, g, b, _ := pal[n].RGBA()
-			println("pal ", n, " = ", r/0x101, g/0x101, b/0x101)
-
-		}
-	} else {
+	if !ok {
 		abend("Not a palletised image")
 	}
 
+	if cmd, ok := commands[command]; ok {
+		err := cmd.cmdHandler(pimg, pal)
+		exitOnError(err)
+	} else {
+		abend("command: " + command + " not found")
+	}
 }
